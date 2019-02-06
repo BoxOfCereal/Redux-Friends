5b8a2266a24fa52ad709686601c79dc8ce36a264
Noah <noah.stoeckel@gmail.com>
2019-02-05 21:57:22 -0500

    Fully Sets Up Initial Environment
    -Creates friendsReducer
    -Creates index of reducers to be used as the rootReducer
    -BUILDS

--- friends/src/index.js
+++ friends/src/index.js
@@ -5,6 +5,13 @@ import App from "./App";
 
 //Import provider than provides the store to the app
 import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+// import logger from "redux-logger";
+// rootReducer from ./reducers
+import rootReducer from "./reducers/index";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
